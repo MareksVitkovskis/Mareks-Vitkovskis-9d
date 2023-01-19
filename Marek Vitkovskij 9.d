aa4b42c115a815866923bb123d905edbdb246ec9
MareksVitkovskis <122967447+MareksVitkovskis@users.noreply.github.com>
2023-01-19 13:12:05 +0200

    Create Marek Vitkovskij 9.d

---
+++ Marek Vitkovskij 9.d
@@ -0,0 +1,98 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int i = 0;
+//	do {
+//		i++;
+//		cout << i << " ";
+//		cout << "Hello" << endl;
+//		cout << i;
+//
+//	} while (i < 115);
+//}
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int i = 0;
+//	do {
+//		i++;
+//		cout << i << " ";
+//		cout << i * i << endl;
+//
+//
+//
+//	} while (i < 20);
+// }
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	//int i = 0;
+//	//do {
+//	//	i++;
+//	//	cout << i << " ";
+//	//	cout << i * i << endl;
+//
+//
+//	//} while (i < 20); */ ;
+//		string password;
+//	int i = 0;
+//	do {
+//		i++;
+//		cin >> password;
+//
+//	} while (password != "1234" && i != 3);
+//}
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	// Local variable declaration:
+//	string myname;
+//
+//
+//	// do loop execution
+//	do {
+//		cout << "Enter my name" << endl;
+//		cin >> myname;
+//		if (myname == "no") { cout << "yes!"; };
+//	} while (myname != "Marek"&&myname!="Vitkovskij");
+//
+//
+//	return 0;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//bool isNumber(string s)
+//{
+//	for (int i = 0; i < s.length(); i++)
+//		if (isdigit(s[i]) == false)
+//		return false;
+//
+//
+//
+//	return true;
+//  }
+//int main () {
+//	//Local variable declaration:
+//	string myname;
+//
+//
+//	// do loop execution 
+//
+//
+//	do {
+//		cout << "Enter my name " << endl;
+//		cin >> myname;
+//
+//		if (isNumber(myname)) { cout << "That is a number!"; };
+//	} while(myname != "Marek");
+//
+//	return 0;
